033ecf27dac7c18cc30b498d00d99f07083553fd
DorelCristian <82279209+DorelCristian@users.noreply.github.com>
2024-03-11 17:45:16 +0200

    Add files via upload

---
+++ proiectMPP/proiectMPP/src/main/java/repo/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using proiectMPP.src.main.java.domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proiectMPP.src.main.java.repo
+{
+    internal interface IRepository<Id,E> where E:Entity<Id>
+    {
+        IEnumerable<E> FindAll();
+        E FindOne(Id id);
+        E Save(E entity);
+        E Delete(Id id);
+        E Update(Id id);
+
+    }
+}
